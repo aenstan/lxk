e94de5037f4a449011c89b28c659302b25058716
gcdd1993 <gcwm99@gmail.com>
2021-08-12 19:03:07 +0800

    fix qinglong version arg

--- build_docker.sh
+++ build_docker.sh
@@ -8,6 +8,6 @@ docker rmi whyour/qinglong:latest || true
 # 清理build 缓存
 docker builder prune
 
-docker build -f Dockerfile -t gcdd1993/qinglong-ninja:latest -t gcdd1993/qinglong-ninja:"$version" .
+docker build -f Dockerfile --build-arg QL_VERSION="$QL_VERSION" -t gcdd1993/qinglong-ninja:latest -t gcdd1993/qinglong-ninja:"$version" .
 docker push gcdd1993/qinglong-ninja:latest
 docker push gcdd1993/qinglong-ninja:"$version"
