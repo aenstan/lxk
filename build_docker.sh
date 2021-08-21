a488945e7f944e3ded136c3464b1182fc39f20f8
gcdd1993 <gcwm99@gmail.com>
2021-08-21 17:04:13 +0800

    不再清理docker build缓存，因为是在Travis CI上运行

--- build_docker.sh
+++ build_docker.sh
@@ -5,9 +5,9 @@ version=$QL_VERSION
 set -ex
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 # 清理docker镜像
-docker rmi whyour/qinglong:latest || true
+#docker rmi whyour/qinglong:latest || true
 # 清理build 缓存
-docker builder prune
+#docker builder prune
 
 docker build -f Dockerfile --build-arg QL_VERSION="$QL_VERSION" -t gcdd1993/qinglong-ninja:latest -t gcdd1993/qinglong-ninja:"$version" .
 docker push gcdd1993/qinglong-ninja:latest
